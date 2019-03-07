6751ad705d44b42a3f6783fc9c6a9196c0416835
LeDuck <joostz14@gmail.com>
2019-03-07 12:49:04 +0100

    Generic algorithm done

--- Assignment/src/Main.kt
+++ Assignment/src/Main.kt
@@ -1,12 +1,59 @@
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
-    val population = 100
-    val individuals = IntRange(1, population).map { createIndividual() }
-    val fitnesses = individuals.map { computeFitness(it) }
-    val parents = selectTwoParents(individuals, fitnesses)
-    val crossover = crossover(parents);
-    println(parents)
+
+    val mutationRate = 0.6
+    val crossoverRate = 0.5
+    val elitism = false
+    val populationSize = 5
+    val numIterations = 50
+
+    var currentPopulation = IntRange(1, populationSize).map { createIndividual() }
+
+    for (i in 0 until numIterations) {
+        val fitness = currentPopulation.map { computeFitness(it) }
+
+        var startIndex = 0
+
+        val nextPopulation = ArrayList<String>()
+
+        if (elitism) {
+            startIndex = 1
+            val populationWithFitness = currentPopulation.zip(fitness)
+            val populationSorted = populationWithFitness.sortedByDescending { it.second }
+            val bestIndividual = populationSorted.first()
+            nextPopulation.add(bestIndividual.first)
+        }
+
+        for (j in startIndex until populationSize) {
+            val parents = selectTwoParents(currentPopulation, fitness)
+
+            var offspring : List<String>
+
+            if(Random.nextDouble() < crossoverRate){
+                offspring = crossover(parents)
+            } else {
+                offspring = parents
+            }
+
+            if(nextPopulation.size < populationSize){
+                nextPopulation.add(mutation(offspring.first(), mutationRate))
+            }
+            if(nextPopulation.size < populationSize){
+                nextPopulation.add(mutation(offspring.last(), mutationRate))
+            }
+        }
+        currentPopulation = nextPopulation
+    }
+
+    val finalFitness = currentPopulation.zip(currentPopulation.map { computeFitness(it) })
+    val bestIndividual = finalFitness.sortedByDescending { it.second }.first().first
+    val bestFitness = finalFitness.sortedByDescending { it.second }.first().second
+    val avg = finalFitness.map { it.second }.average()
+
+    println("Best fitness = $bestFitness")
+    println("Best individual = $bestIndividual")
+    println("Average fitness = $avg")
 }
 
 fun createIndividual(): String {
@@ -55,4 +102,21 @@ fun crossover(parents: List<String>): List<String> {
         parents[0].substring(0, pointer) + parents[1].substring(pointer, l),
         parents[1].substring(0, pointer) + parents[0].substring(pointer, l)
     )
+}
+
+fun mutation(individual: String, mutationRate: Double): String {
+    var output = ""
+    for (char in individual) {
+        val rnd = Random.nextDouble(1.0)
+        output += if (rnd <= mutationRate) {
+            if (char == '1') {
+                "0"
+            } else {
+                "1"
+            }
+        } else {
+            char
+        }
+    }
+    return output
 }
