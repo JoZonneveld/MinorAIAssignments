f055b0b899d3a3daabb953c665aafe907959b312
Joost Zonneveld <0924851@hr.nl>
2019-04-04 12:57:12 +0200

    C#

---
+++ Assignment/C#/Genetic/Algorithm.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Linq;
+
+namespace Genetic
+{
+    class Algorithm<A>
+    {
+        double crossoverRate;
+        double mutationRate;
+        bool elitism;
+        int populationSize;
+        int numIterations;
+        Random r = new Random();
+
+        public Algorithm(double crossoverRate, double mutationRate, bool elitism, int populationSize, int numIterations)
+        {
+            this.crossoverRate = crossoverRate;
+            this.mutationRate = mutationRate;
+            this.elitism = elitism;
+            this.populationSize = populationSize;
+            this.numIterations = numIterations;
+        }
+
+        public A Run(Func<A> createAividual, Func<A,double> computeFitness, Func<A[],double[],Func<Tuple<A,A>>> selectTwoParents, 
+            Func<Tuple<A,A>,Tuple<A,A>> crossover, Func<A, double, A> mutation)
+        {
+            // initialize the first population
+            var initialPopulation = Enumerable.Range(0, populationSize).Select(i => createAividual()).ToArray();
+
+            var currentPopulation = initialPopulation;
+            
+            for (int generation = 0; generation < numIterations; generation++)
+            {
+                // compute fitness of each individual in the population
+                var fitnesses = Enumerable.Range(0, populationSize).Select(i => computeFitness(currentPopulation[i])).ToArray();
+
+                var nextPopulation = new A[populationSize]; 
+
+                // apply elitism
+                int startAex; 
+                if(elitism)
+                {
+                    startAex = 1;
+                    var populationWithFitness = currentPopulation.Select((individual, index) => new Tuple<A,double>(individual,fitnesses[index]));
+                    var populationSorted = populationWithFitness.OrderByDescending(tuple => tuple.Item2); // item2 is the fitness
+                    var bestAividual = populationSorted.First();
+                    nextPopulation[0] = bestAividual.Item1;
+                } else
+                {
+                    startAex = 0;
+                }
+
+                // initialize the selection function given the current individuals and their fitnesses
+                var getTwoParents = selectTwoParents(currentPopulation, fitnesses);
+                
+                // create the individuals of the next generation
+                for (int newA = startAex; newA < populationSize; newA++)
+                {
+                    // select two parents
+                    var parents = getTwoParents();
+                    
+                    // do a crossover between the selected parents to generate two children (with a certain probability, crossover does not happen and the two parents are kept unchanged)
+                    Tuple<A,A> offspring;
+                    if (r.NextDouble() < crossoverRate)
+                        offspring = crossover(parents);
+                    else
+                        offspring = parents;
+
+                    // save the two children in the next population (after mutation)
+                    nextPopulation[newA++] = mutation(offspring.Item1, mutationRate);
+                    if (newA < populationSize) //there is still space for the second children inside the population
+                        nextPopulation[newA] = mutation(offspring.Item2, mutationRate);
+                }
+
+                // the new population becomes the current one
+                currentPopulation = nextPopulation;
+
+                // in case it's needed, check here some convergence condition to terminate the generations loop earlier
+            }
+
+            // recompute the fitnesses on the final population and return the best individual
+            var finalFitnesses = Enumerable.Range(0, populationSize).Select(i => computeFitness(currentPopulation[i])).ToArray();
+            return currentPopulation.Select((individual, index) => new Tuple<A, double>(individual, finalFitnesses[index])).OrderByDescending(tuple => tuple.Item2).First().Item1;
+        }
+    }
+}
--- Assignment/Clustering/src/Main-Clustering.kt
+++ Assignment/Clustering/src/Main-Clustering.kt
@@ -26,46 +26,108 @@ fun main(args: Array<String>) {
         }
     }
 
-    val centroids = computeRandomCentroids(5)
-    val t = clientWineChoices.map { it.second }.forEach { dist(centroids, it)}
-
-    println()
-
-}
-
-fun dist(centroids: List<Double>, wineChoices : List<WineChoice>) {
-    val dataValues = wineChoices.map { it.choice }
-    val zippedValues = dataValues.zip(centroids).map { Math.pow(it.first - it.second, 2.0) }.sum()
-    val res = Math.sqrt(zippedValues)
-    println()
-}
-
-fun <A> clustering(k : Int, iterations: Int, dataset: A){
-
-
-
-    var centroids = computeRandomCentroids(k)
-    val oldCentroids: MutableList<List<A>> = mutableListOf()
-    var i = 0
-    while(i < iterations && centroids !in oldCentroids){
-        i++
+    val dist: (List<WineChoice>, List<WineChoice>) -> Double = { centroid, data ->
+        val dataValues = data.map { it.choice }
+        val centroidValues = centroid.map { it.choice }
+        val zippedValues = dataValues.zip(centroidValues).map { Math.pow(it.first - it.second, 2.0) }.sum()
+        Math.sqrt(zippedValues)
     }
+
+
+
+    val res = kMeans(
+            k = 5,
+            iterations = 10000,
+            dataset = clientWineChoices.map { it.second },
+            initialize = {k, dataset ->
+                val values = dataset.first().map { it.offerId }
+                IntRange(1, k).map {
+                    values.map { offerId ->
+                        WineChoice(
+                                offerId,
+                                Random.nextDouble()
+                        )
+                    }
+                }
+            },
+            calculateDistance = dist,
+            calculateMeanCentroid = {centroid, dataset ->
+                val groupedOffers = dataset.flatten().groupBy { it.offerId }
+                centroid.map {
+                    WineChoice(
+                            it.offerId,
+                            requireNotNull(groupedOffers[it.offerId]).map { client -> client.choice }.sum().div(dataset.size.toDouble())
+                    )
+                }
+            }
+    )
+
+    val clientClosestToCluster = clientWineChoices.map { client ->
+        val c = res.withIndex().map { (index, centroid) -> index to dist(centroid, client.second) }
+        val b = c.sortedBy { it.second }
+        val a = b.first().first
+        client to a
+    }
+
+    res.withIndex().forEach { (index, it) ->
+        println("# Cluster ${index + 1}")
+        println("|Offer #|Choice|Wine|")
+        println("|:-|:-|:-|")
+
+        val sorted = it.map { wine ->
+            wine.offerId to clientClosestToCluster
+                    .filter { (_, clientIndex) -> clientIndex == index }
+                    .sumByDouble { (pair, _) ->
+                        val (_, values) = pair
+                        values.filter { it.offerId == wine.offerId }.sumByDouble { it.choice }
+                    }
+        }.sortedByDescending { it.second }
+        sorted.forEach { (offerId, choice) ->
+            val wine = wines.find { it.offerId == offerId }!!.varietal
+            println("|$offerId|$choice|$wine|")
+        }
+    }
+    println()
 }
 
-fun computeRandomCentroids(k: Int): List<Double> {
-    val centroids = mutableListOf<Double>()
 
-    for(i in 0 until k){
-        centroids.add(Random.nextDouble(1.0))
+fun <T> kMeans(
+        k: Int,
+        iterations: Int,
+        dataset: List<T>,
+        initialize: (Int, List<T>) -> List<T>,
+        calculateDistance: (T, T) -> Double,
+        calculateMeanCentroid: (T, List<T>) -> T
+): List<T> {
+    val centroidsMap: HashMap<T, MutableList<T>> = HashMap()
+
+    var centroids = initialize(k, dataset)
+    val oldCentroids: MutableList<List<T>> = mutableListOf()
+
+    var currentIteration = 0
+    while (currentIteration < iterations && centroids !in oldCentroids) {
+        currentIteration++
+        oldCentroids.add(centroids)
+
+        centroidsMap.clear()
+        // for each data point
+        dataset.forEach { data ->
+            // - find nearest centroid
+            val nearest = centroids.sortedBy { centroid -> calculateDistance(centroid, data) }.first()
+            // - assign the point to that cluster
+            centroidsMap[nearest]?.run { this.add(data) } ?: run { centroidsMap[nearest] = mutableListOf(data) }
+        }
+
+        // for each cluster
+        centroids = centroids.map { centroid ->
+            // - new centroid = mean of all points assigned to that cluster
+            centroidsMap[centroid]?.let { list ->
+                calculateMeanCentroid(centroid, list)
+            } ?: centroid
+        }
     }
 
     return centroids
-}
-
-fun <A> reComputeCentroid(centroid : A, cluster: List<A>) : A {
-
-
-    return centroid
 }
 
 class WineChoice(val offerId : Int, val choice: Double)
